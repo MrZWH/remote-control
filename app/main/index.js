33c731d3e1f2113fa562b4410332d5b910942357
MrZWH <blesszhang@foxmail.com>
2020-06-03 13:13:10 +0800

    :sparkles: 初始化项目工程

---
+++ app/main/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let win;
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 600,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadURL(path.resolve(__dirname, '../renderer/pages/main/index.html'));
+  }
+});
